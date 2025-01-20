e169c894d16f14d0464d1d4122dc41aed770d4f6
hujun <18660093805@163.com>
2025-01-20 19:38:30 +0800

    perf: change llm model

--- script/rag_service_test.sh
+++ script/rag_service_test.sh
@@ -2,7 +2,7 @@ curl --location 'http://127.0.0.1:18888/v1/chat/completions' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Bearer sk-67hBSTsaf0qqvpTN2eA5A4433c2343D3867d0f74D8F0322' \
 --data '{
-  "model": "gpt-4o-mini-2024-07-18",
+  "model": "gpt-4o-mini",
   "messages": [
     {
       "role": "user",
